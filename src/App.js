d52b42fca2024b17bbfa237132ccf1d0eaa8020f
shantanuborade007 <shantanuborade007@gmail.com>
2024-01-03 20:48:49 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -11,17 +11,18 @@ function App() {
     setCurrentTab(newValue);
   };
   const allData = [
-  { id:1,CCLtStatus: 'status1', counterpartName: 'counterpart1', SDSId: 'id1', marketTier: 'tier1', LEI: 'lei1', reporting: 'reporting1', tableType: 'type1' },
-  { id:2,CCLtStatus: 'status2', counterpartName: 'counterpart2', SDSId: 'id2', marketTier: 'tier2', LEI: 'lei2', reporting: 'reporting2', tableType: 'type2' },
-  { id:3,CCLtStatus: 'status3', counterpartName: 'counterpart3', SDSId: 'id3', marketTier: 'tier3', LEI: 'lei3', reporting: 'reporting3', tableType: 'type1' },
-  { id:4,CCLtStatus: 'status4', counterpartName: 'counterpart4', SDSId: 'id4', marketTier: 'tier4', LEI: 'lei4', reporting: 'reporting4', tableType: 'type2' },
-  { id:5,CCLtStatus: 'status5', counterpartName: 'counterpart5', SDSId: 'id5', marketTier: 'tier5', LEI: 'lei5', reporting: 'reporting5', tableType: 'type1' },
-  { id:6,CCLtStatus: 'status6', counterpartName: 'counterpart6', SDSId: 'id6', marketTier: 'tier6', LEI: 'lei6', reporting: 'reporting6', tableType: 'type2' },
-  { id:7,CCLtStatus: 'status7', counterpartName: 'counterpart7', SDSId: 'id7', marketTier: 'tier7', LEI: 'lei7', reporting: 'reporting7', tableType: 'type1' },
-  { id:8,CCLtStatus: 'status8', counterpartName: 'counterpart8', SDSId: 'id8', marketTier: 'tier8', LEI: 'lei8', reporting: 'reporting8', tableType: 'type2' },
-  { id:9,CCLtStatus: 'status9', counterpartName: 'counterpart9', SDSId: 'id9', marketTier: 'tier9', LEI: 'lei9', reporting: 'reporting9', tableType: 'type1' },
-  { id:10,CCLtStatus: 'status10', counterpartName: 'counterpart10', SDSId: 'id10', marketTier: 'tier10', LEI: 'lei10', reporting: 'reporting10', tableType: 'type2' },
+    { id: 1, CCLtStatus: 'status1', counterpartName: 'counterpart1', SDSId: 'id1', marketTier: 'tier1', LEI: 'lei1', reporting: 'reporting1', tableType: 'type1', stage: 'demoStage1', margin: 'demoMargin1', 'ball-in': 'demoBallIn1', riskmitigation: 'demoRiskMitigation1', 'cftc_p1_p2': 'demoCFTC1', 'global_kyc': 'demoGlobalKYC1', 'global_retail_client': 'demoGlobalRetailClient1' },
+    { id: 2, CCLtStatus: 'status2', counterpartName: 'counterpart2', SDSId: 'id2', marketTier: 'tier2', LEI: 'lei2', reporting: 'reporting2', tableType: 'type2', stage: 'demoStage2', margin: 'demoMargin2', 'ball-in': 'demoBallIn2', riskmitigation: 'demoRiskMitigation2', 'cftc_p1_p2': 'demoCFTC2', 'global_kyc': 'demoGlobalKYC2', 'global_retail_client': 'demoGlobalRetailClient2' },
+    { id: 3, CCLtStatus: 'status3', counterpartName: 'counterpart3', SDSId: 'id3', marketTier: 'tier3', LEI: 'lei3', reporting: 'reporting3', tableType: 'type1', stage: 'demoStage3', margin: 'demoMargin3', 'ball-in': 'demoBallIn3', riskmitigation: 'demoRiskMitigation3', 'cftc_p1_p2': 'demoCFTC3', 'global_kyc': 'demoGlobalKYC3', 'global_retail_client': 'demoGlobalRetailClient3' },
+    { id: 4, CCLtStatus: 'status4', counterpartName: 'counterpart4', SDSId: 'id4', marketTier: 'tier4', LEI: 'lei4', reporting: 'reporting4', tableType: 'type2', stage: 'demoStage4', margin: 'demoMargin4', 'ball-in': 'demoBallIn4', riskmitigation: 'demoRiskMitigation4', 'cftc_p1_p2': 'demoCFTC4', 'global_kyc': 'demoGlobalKYC4', 'global_retail_client': 'demoGlobalRetailClient4' },
+    { id: 5, CCLtStatus: 'status5', counterpartName: 'counterpart5', SDSId: 'id5', marketTier: 'tier5', LEI: 'lei5', reporting: 'reporting5', tableType: 'type1', stage: 'demoStage5', margin: 'demoMargin5', 'ball-in': 'demoBallIn5', riskmitigation: 'demoRiskMitigation5', 'cftc_p1_p2': 'demoCFTC5', 'global_kyc': 'demoGlobalKYC5', 'global_retail_client': 'demoGlobalRetailClient5' },
+    { id: 6, CCLtStatus: 'status6', counterpartName: 'counterpart6', SDSId: 'id6', marketTier: 'tier6', LEI: 'lei6', reporting: 'reporting6', tableType: 'type2', stage: 'demoStage6', margin: 'demoMargin6', 'ball-in': 'demoBallIn6', riskmitigation: 'demoRiskMitigation6', 'cftc_p1_p2': 'demoCFTC6', 'global_kyc': 'demoGlobalKYC6', 'global_retail_client': 'demoGlobalRetailClient6' },
+    { id: 7, CCLtStatus: 'status7', counterpartName: 'counterpart7', SDSId: 'id7', marketTier: 'tier7', LEI: 'lei7', reporting: 'reporting7', tableType: 'type1', stage: 'demoStage7', margin: 'demoMargin7', 'ball-in': 'demoBallIn7', riskmitigation: 'demoRiskMitigation7', 'cftc_p1_p2': 'demoCFTC7', 'global_kyc': 'demoGlobalKYC7', 'global_retail_client': 'demoGlobalRetailClient7' },
+    { id: 8, CCLtStatus: 'status8', counterpartName: 'counterpart8', SDSId: 'id8', marketTier: 'tier8', LEI: 'lei8', reporting: 'reporting8', tableType: 'type2', stage: 'demoStage8', margin: 'demoMargin8', 'ball-in': 'demoBallIn8', riskmitigation: 'demoRiskMitigation8', 'cftc_p1_p2': 'demoCFTC8', 'global_kyc': 'demoGlobalKYC8', 'global_retail_client': 'demoGlobalRetailClient8' },
+    { id: 9, CCLtStatus: 'status9', counterpartName: 'counterpart9', SDSId: 'id9', marketTier: 'tier9', LEI: 'lei9', reporting: 'reporting9', tableType: 'type1', stage: 'demoStage9', margin: 'demoMargin9', 'ball-in': 'demoBallIn9', riskmitigation: 'demoRiskMitigation9', 'cftc_p1_p2': 'demoCFTC9', 'global_kyc': 'demoGlobalKYC9', 'global_retail_client': 'demoGlobalRetailClient9' },
+    { id: 10, CCLtStatus: 'status10', counterpartName: 'counterpart10', SDSId: 'id10', marketTier: 'tier10', LEI: 'lei10', reporting: 'reporting10', tableType: 'type2', stage: 'demoStage10', margin: 'demoMargin10', 'ball-in': 'demoBallIn10', riskmitigation: 'demoRiskMitigation10', 'cftc_p1_p2': 'demoCFTC10', 'global_kyc': 'demoGlobalKYC10', 'global_retail_client': 'demoGlobalRetailClient10' },
   ];
+  
 
   const redData = allData.filter(row => row.tableType === 'type1');
   const greenData = allData.filter(row => row.tableType !== 'type1');
--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -1,31 +1,74 @@
 import * as React from 'react';
-import { DataGrid,GridToolbar  } from '@mui/x-data-grid';
+import { DataGridPro, GridToolbar } from '@mui/x-data-grid-pro';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+// ORIGINAL 
 
 function Table({ data }) {
-    const columns = [
-        { field: 'CCLtStatus', headerName: 'CCLt Status', sortable: true, filter: true, width: 150, 
-          headerClassName: 'lightblue-header', cellClassName: 'border-right' },
-        { field: 'counterpartName', headerName: 'Counterpart Name', sortable: true, filter: true, width: 150, 
-          headerClassName: 'lightblue-header', cellClassName: 'border-right' },
-        { field: 'SDSId', headerName: 'SDS Id', sortable: true, filter: true, width: 150, 
-          headerClassName: 'lightblue-header', cellClassName: 'border-right' },
-        { field: 'marketTier', headerName: 'Market Tier', sortable: true, filter: true, width: 150, 
-          headerClassName: 'lightblue-header', cellClassName: 'border-right' },
-        { field: 'LEI', headerName: 'LEI', sortable: true, filter: true, width: 150, 
-          headerClassName: 'lightblue-header', cellClassName: 'border-right' },
-        { field: 'reporting', headerName: 'Reporting', sortable: true, filter: true, width: 150, 
-          headerClassName: 'lightblue-header', cellClassName: 'border-right' },
-      ];
+
+const [cellInfo, setCellInfo] = React.useState(null);
+
+ const handleCellClick = (params, event) => {
+   setCellInfo(params);
+ };
+
+
+  
+  console.log(data);
+  const columns = [
+    { field: 'CCLtStatus', headerName: 'CCLt Status', sortable: true, filter: true, width: 150, headerClassName: 'lightblue-header', cellClassName: 'border-right', fixed: 'left' },
+    { field: 'counterpartName', headerName: 'Counterpart Name', sortable: true, filter: true, width: 150, headerClassName: 'lightblue-header', cellClassName: 'border-right', fixed: 'left' },
+    { field: 'SDSId', headerName: 'SDS Id', sortable: true, filter: true, width: 150, headerClassName: 'lightblue-header', cellClassName: 'border-right', fixed: 'left' },
+    { field: 'marketTier', headerName: 'Market Tier', sortable: true, filter: true, width: 150, headerClassName: 'lightblue-header', cellClassName: 'border-right' },
+    { field: 'LEI', headerName: 'LEI', sortable: true, filter: true, width: 150, headerClassName: 'lightblue-header', cellClassName: 'border-right' },
+    { field: 'reporting', headerName: 'Reporting', sortable: true, filter: true, width: 150, headerClassName: 'lightblue-header', cellClassName: 'border-right' },
+    {
+      field: 'typeIndicator',
+      headerName: 'Type Indicator',
+      sortable: false,
+      filter: false,
+      width: 150,
+      headerClassName: 'lightblue-header',
+      cellClassName: 'border-right',
+      renderCell: (params) => {
+        if (params.row.tableType === 'type1') {
+          return <span style={{ color: 'red' }}>R</span>;
+        } else if (params.row.tableType === 'type2') {
+          return <span style={{ color: 'green' }}>G</span>;
+        } else {
+          return <span style={{ color: 'blue' }}>Unknown</span>;
+        }
+      },
+    },
+    { field: 'stage', headerName: 'Stage', sortable: true, filter: true, width: 150, headerClassName: 'lightblue-header', cellClassName: 'border-right' },
+    { field: 'margin', headerName: 'Margin', sortable: true, filter: true, width: 150, headerClassName: 'lightblue-header', cellClassName: 'border-right' },
+    { field: 'ball-in', headerName: 'Ball-In', sortable: true, filter: true, width: 150, headerClassName: 'lightblue-header', cellClassName: 'border-right' },
+    { field: 'riskmitigation', headerName: 'Risk Mitigation', sortable: true, filter: true, width: 150, headerClassName: 'lightblue-header', cellClassName: 'border-right' },
+    { field: 'cftc_p1_p2', headerName: 'CFTC P1&P2', sortable: true, filter: true, width: 150, headerClassName: 'lightblue-header', cellClassName: 'border-right' },
+    { field: 'global_kyc', headerName: 'Global KYC', sortable: true, filter: true, width: 150, headerClassName: 'lightblue-header', cellClassName: 'border-right' },
+    { field: 'global_retail_client', headerName: 'Global Retail Client', sortable: true, filter: true, width: 150, headerClassName: 'lightblue-header', cellClassName: 'border-right' },
+  ];
 
   return (
-    <div style={{ height: 400, width: '75%' }}>
-      <DataGrid
+    <div style={{ height: '100vh', width: '100%',display:'flex' }}>
+      <DataGridPro
         rows={data}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
-        checkboxSelection
+        // checkboxSelection
+        autoHeight
+        autoPageSize
         slots={{ toolbar: GridToolbar }}
+         onCellClick={handleCellClick}
+      />
+
+      <Autocomplete
+      id="combo-box-demo"
+      options={Array.from(new Set(data.map((row) => row[cellInfo?.field]))).sort()}
+      getOptionLabel={(option) => option}
+      style={{ width: 300 }}
+      renderInput={(params) => <TextField {...params} label={cellInfo ? cellInfo.field : ''} variant="outlined" />}
       />
     </div>
   );
